9c9b939706060f6382ef68e2fed68b4a06339a5b
Jonas Thaun <thaunjonas@gmail.com>
2026-08-04 21:25:11 +0200

    fmc init

--- lvgl-qspi/Core/Src/gpio.c
+++ lvgl-qspi/Core/Src/gpio.c
@@ -35,8 +35,6 @@
 /** Configure pins
      PE4   ------> LTDC_B0
      PG14   ------> ETH_TXD1
-     PE1   ------> FMC_NBL1
-     PE0   ------> FMC_NBL0
      PB8   ------> I2C1_SCL
      PB5   ------> USB_OTG_HS_ULPI_D7
      PB4   ------> S_TIM3_CH1
@@ -48,10 +46,8 @@
      PG13   ------> ETH_TXD0
      PB9   ------> I2C1_SDA
      PB7   ------> USART1_RX
-     PG15   ------> FMC_SDNCAS
      PG11   ------> ETH_TX_EN
      PJ13   ------> LTDC_B1
-     PD0   ------> FMC_D2_DA2
      PC11   ------> SDMMC1_D3
      PC10   ------> SDMMC1_D2
      PA12   ------> USB_OTG_FS_DP
@@ -63,9 +59,7 @@
      PG10   ------> SAI2_SD_B
      PJ14   ------> LTDC_B2
      PD3   ------> DCMI_D5
-     PD1   ------> FMC_D3_DA3
      PA11   ------> USB_OTG_FS_DM
-     PF0   ------> FMC_A0
      PI5   ------> SAI2_SCK_A
      PI7   ------> SAI2_FS_A
      PI10   ------> LTDC_HSYNC
@@ -76,7 +70,6 @@
      PD2   ------> SDMMC1_CMD
      PI1   ------> SPI2_SCK
      PA10   ------> USB_OTG_FS_ID
-     PF1   ------> FMC_A1
      PI9   ------> LTDC_VSYNC
      PH14   ------> DCMI_D4
      PI0   ------> S_TIM5_CH4
@@ -85,45 +78,29 @@
      PK2   ------> LTDC_G7
      PC9   ------> SDMMC1_D1
      PA8   ------> S_TIM1_CH1
-     PF2   ------> FMC_A2
      PI15   ------> LTDC_R0
      PJ11   ------> LTDC_G4
      PK0   ------> LTDC_G5
      PC8   ------> SDMMC1_D0
      PC7   ------> USART6_RX
-     PF3   ------> FMC_A3
      PI14   ------> LTDC_CLK
      PH4   ------> USB_OTG_HS_ULPI_NXT
      PJ8   ------> LTDC_G1
      PJ10   ------> LTDC_G3
-     PG8   ------> FMC_SDCLK
      PC6   ------> USART6_TX
-     PF4   ------> FMC_A4
-     PH5   ------> FMC_SDNWE
-     PH3   ------> FMC_SDNE0
      PJ7   ------> LTDC_G0
      PJ9   ------> LTDC_G2
      PF7   ------> ADC3_IN5
      PF6   ------> ADC3_IN4
-     PF5   ------> FMC_A5
      PJ6   ------> LTDC_R7
-     PD15   ------> FMC_D1_DA1
      PB13   ------> USB_OTG_HS_ULPI_D6
-     PD10   ------> FMC_D15_DA15
      PF10   ------> ADC3_IN8
      PF9   ------> ADC3_IN7
      PF8   ------> ADC3_IN6
-     PC3   ------> FMC_SDCKE0
-     PD14   ------> FMC_D0_DA0
      PB12   ------> USB_OTG_HS_ULPI_D5
-     PD9   ------> FMC_D14_DA14
-     PD8   ------> FMC_D13_DA13
      PC0   ------> USB_OTG_HS_ULPI_STP
      PC1   ------> ETH_MDC
      PC2   ------> USB_OTG_HS_ULPI_DIR
-     PF12   ------> FMC_A6
-     PG1   ------> FMC_A11
-     PF15   ------> FMC_A9
      PJ4   ------> LTDC_R5
      PJ5   ------> LTDC_R6
      PH12   ------> DCMI_D3
@@ -131,12 +108,7 @@
      PA0/WKUP   ------> ADCx_IN0
      PA4   ------> DCMI_HSYNC
      PC4   ------> ETH_RXD0
-     PF13   ------> FMC_A7
-     PG0   ------> FMC_A10
      PJ3   ------> LTDC_R4
-     PE8   ------> FMC_D5_DA5
-     PG5   ------> FMC_A15_BA1
-     PG4   ------> FMC_A14_BA0
      PH7   ------> I2C3_SCL
      PH9   ------> DCMI_D0
      PH11   ------> DCMI_D2
@@ -144,12 +116,7 @@
      PA6   ------> DCMI_PIXCLK
      PA5   ------> USB_OTG_HS_ULPI_CK
      PC5   ------> ETH_RXD1
-     PF14   ------> FMC_A8
      PJ2   ------> LTDC_R3
-     PF11   ------> FMC_SDNRAS
-     PE9   ------> FMC_D6_DA6
-     PE11   ------> FMC_D8_DA8
-     PE14   ------> FMC_D11_DA11
      PB10   ------> USB_OTG_HS_ULPI_D3
      PH6   ------> S_TIM12_CH1
      PH8   ------> I2C3_SDA
@@ -160,11 +127,6 @@
      PB0   ------> USB_OTG_HS_ULPI_D1
      PJ0   ------> LTDC_R1
      PJ1   ------> LTDC_R2
-     PE7   ------> FMC_D4_DA4
-     PE10   ------> FMC_D7_DA7
-     PE12   ------> FMC_D9_DA9
-     PE15   ------> FMC_D12_DA12
-     PE13   ------> FMC_D10_DA10
      PB11   ------> USB_OTG_HS_ULPI_D4
      PB14   ------> SPI2_MISO
      PB15   ------> SPI2_MOSI
@@ -223,18 +185,6 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
   HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : FMC_NBL1_Pin FMC_NBL0_Pin FMC_D5_Pin FMC_D6_Pin
-                           FMC_D8_Pin FMC_D11_Pin FMC_D4_Pin FMC_D7_Pin
-                           FMC_D9_Pin FMC_D12_Pin FMC_D10_Pin */
-  GPIO_InitStruct.Pin = FMC_NBL1_Pin|FMC_NBL0_Pin|FMC_D5_Pin|FMC_D6_Pin
-                          |FMC_D8_Pin|FMC_D11_Pin|FMC_D4_Pin|FMC_D7_Pin
-                          |FMC_D9_Pin|FMC_D12_Pin|FMC_D10_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
-  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
   /*Configure GPIO pins : ARDUINO_SCL_D15_Pin ARDUINO_SDA_D14_Pin */
   GPIO_InitStruct.Pin = ARDUINO_SCL_D15_Pin|ARDUINO_SDA_D14_Pin;
@@ -304,16 +254,6 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
   HAL_GPIO_Init(VCP_RX_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : FMC_SDNCAS_Pin FMC_SDCLK_Pin FMC_A11_Pin FMC_A10_Pin
-                           FMC_BA1_Pin FMC_BA0_Pin */
-  GPIO_InitStruct.Pin = FMC_SDNCAS_Pin|FMC_SDCLK_Pin|FMC_A11_Pin|FMC_A10_Pin
-                          |FMC_BA1_Pin|FMC_BA0_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
-  HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
-
   /*Configure GPIO pins : LCD_B1_Pin LCD_B2_Pin LCD_B3_Pin LCD_G4_Pin
                            LCD_G1_Pin LCD_G3_Pin LCD_G0_Pin LCD_G2_Pin
                            LCD_R7_Pin LCD_R5_Pin LCD_R6_Pin LCD_R4_Pin
@@ -339,16 +279,6 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(Audio_INT_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : FMC_D2_Pin FMC_D3_Pin FMC_D1_Pin FMC_D15_Pin
-                           FMC_D0_Pin FMC_D14_Pin FMC_D13_Pin */
-  GPIO_InitStruct.Pin = FMC_D2_Pin|FMC_D3_Pin|FMC_D1_Pin|FMC_D15_Pin
-                          |FMC_D0_Pin|FMC_D14_Pin|FMC_D13_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
-  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
   /*Configure GPIO pins : OTG_FS_P_Pin OTG_FS_N_Pin OTG_FS_ID_Pin */
   GPIO_InitStruct.Pin = OTG_FS_P_Pin|OTG_FS_N_Pin|OTG_FS_ID_Pin;
@@ -419,18 +349,6 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(uSD_Detect_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : FMC_A0_Pin FMC_A1_Pin FMC_A2_Pin FMC_A3_Pin
-                           FMC_A4_Pin FMC_A5_Pin FMC_A6_Pin FMC_A9_Pin
-                           FMC_A7_Pin FMC_A8_Pin FMC_SDNRAS_Pin */
-  GPIO_InitStruct.Pin = FMC_A0_Pin|FMC_A1_Pin|FMC_A2_Pin|FMC_A3_Pin
-                          |FMC_A4_Pin|FMC_A5_Pin|FMC_A6_Pin|FMC_A9_Pin
-                          |FMC_A7_Pin|FMC_A8_Pin|FMC_SDNRAS_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
-  HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LCD_HSYNC_Pin LCD_VSYNC_Pin LCD_R0_Pin LCD_CLK_Pin */
   GPIO_InitStruct.Pin = LCD_HSYNC_Pin|LCD_VSYNC_Pin|LCD_R0_Pin|LCD_CLK_Pin;
@@ -546,14 +464,6 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Alternate = GPIO_AF10_OTG_HS;
   HAL_GPIO_Init(ULPI_NXT_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : FMC_SDNME_Pin FMC_SDNE0_Pin */
-  GPIO_InitStruct.Pin = FMC_SDNME_Pin|FMC_SDNE0_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
-  HAL_GPIO_Init(GPIOH, &GPIO_InitStruct);
-
   /*Configure GPIO pins : ARDUINO_D4_Pin ARDUINO_D2_Pin EXT_RST_Pin */
   GPIO_InitStruct.Pin = ARDUINO_D4_Pin|ARDUINO_D2_Pin|EXT_RST_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
@@ -568,14 +478,6 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : FMC_SDCKE0_Pin */
-  GPIO_InitStruct.Pin = FMC_SDCKE0_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF12_FMC;
-  HAL_GPIO_Init(FMC_SDCKE0_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : ULPI_STP_Pin ULPI_DIR_Pin */
   GPIO_InitStruct.Pin = ULPI_STP_Pin|ULPI_DIR_Pin;
--- lvgl-qspi/Core/Src/main.c
+++ lvgl-qspi/Core/Src/main.c
@@ -20,6 +20,7 @@
 #include "main.h"
 #include "quadspi.h"
 #include "gpio.h"
+#include "fmc.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -54,8 +55,6 @@ const uint32_t qspi_test_zahl __attribute__((section(".qspi_data"))) = 0xDEADBEE
 
 /* USER CODE END PV */
 
-/* USER CODE END PV */
-
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MPU_Config(void);
@@ -69,9 +68,9 @@ static void MPU_Config(void);
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
 
@@ -80,7 +79,7 @@ int main(void)
   /* USER CODE END 1 */
 
   /* MPU Configuration--------------------------------------------------------*/
-  // MPU_Config();
+  MPU_Config();
 
   /* MCU Configuration--------------------------------------------------------*/
 
@@ -101,6 +100,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_QUADSPI_Init();
+  MX_FMC_Init();
   /* USER CODE BEGIN 2 */
 
   QSPI_CommandTypeDef sCommand = {0};
@@ -139,22 +139,22 @@ int main(void)
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-   */
+  */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-   * in the RCC_OscInitTypeDef structure.
-   */
+  * in the RCC_OscInitTypeDef structure.
+  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -169,15 +169,16 @@ void SystemClock_Config(void)
   }
 
   /** Activate the Over-Drive mode
-   */
+  */
   if (HAL_PWREx_EnableOverDrive() != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-   */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
@@ -193,7 +194,7 @@ void SystemClock_Config(void)
 
 /* USER CODE END 4 */
 
-/* MPU Configuration */
+ /* MPU Configuration */
 
 void MPU_Config(void)
 {
@@ -203,7 +204,7 @@ void MPU_Config(void)
   HAL_MPU_Disable();
 
   /** Initializes and configures the Region and the memory to be protected
-   */
+  */
   MPU_InitStruct.Enable = MPU_REGION_ENABLE;
   MPU_InitStruct.Number = MPU_REGION_NUMBER0;
   MPU_InitStruct.BaseAddress = 0x0;
@@ -219,12 +220,13 @@ void MPU_Config(void)
   HAL_MPU_ConfigRegion(&MPU_InitStruct);
   /* Enables the MPU */
   HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+
 }
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -237,12 +239,12 @@ void Error_Handler(void)
 }
 #ifdef USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
